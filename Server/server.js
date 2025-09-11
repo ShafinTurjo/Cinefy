49d9fd05601c1519b0234e59e98bcb1a1e63cc38
Dola138 <dola.cse.20230104138@aust.edu>
2025-09-11 09:37:26 +0600

    cccc

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import showRouter from './Routes/showRoutes.js';
-import bookingRouter from './Routes/bookingRoutes.js';
+
 const app = express();
 const port = process.env.PORT||5000;
 
@@ -19,14 +19,14 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => res.send('Server is Live!'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('/api/shows', showRouter);
-app.use('/api/bookings', bookingRouter);
+
 
 const startServer = async () => {
   try {
 // Connect to database
 await connectDB();
   app.listen(port, () => {
-    console.log(`🚀 Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
   });
 }catch (error) {
     console.error('Failed to start server:', error);
